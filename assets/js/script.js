9622dab9e77e4ac788367ffb53f3c1b14614fe61
AnnickRynne <annickrynne@gmail.com>
2022-02-21 19:36:10 +0000

    Add the division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,11 @@ function runGame(gameType) {
         else if (gameType === "substract") {
             displaySubstractQuestion(num1, num2);
         }
+        else if (gameType === "division") {
+            let num1 = Math.floor(Math.random() * 100) + 1;
+            let num2 = Math.floor(Math.random() * 10) + 1;
+            displayDivisionQuestion(num1, num2);
+        }
         else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -91,6 +96,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "substract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     }
         else {
         alert(`Unimplemented operator ${operator}`);
@@ -132,4 +139,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
