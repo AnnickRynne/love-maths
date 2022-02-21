df182af5d33e073328a2c81bdb221e4afdf0610c
AnnickRynne <annickrynne@gmail.com>
2022-02-21 10:03:34 +0000

    Add mulitiplication game code: Add to runGame, calculateAnswer and displayMultiplyQuestion functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,12 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } 
+        else if (gameType === "multiply") {
+            displayMultiplyQuestion(num1, num2);
+        }
+
+        else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -69,7 +74,9 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    }else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+        } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${gameType}. Aborting!`;
     }
@@ -103,6 +110,8 @@ function displaySubstractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
